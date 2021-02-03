d99614cbfc03b5193317eda7a35eda2d666daf69
YilmazHeleteli <59505029+YilmazHeleteli@users.noreply.github.com>
2021-02-03 14:27:38 +0000

    Added docks

--- src/javagame/Quest.java
+++ src/javagame/Quest.java
@@ -3,8 +3,9 @@ import org.newdawn.slick.*;
 
 
 public class Quest {
-	Image quest;
-	
+	Image questLog;
+	String questname;
+	Image questDesc;
 	public void displayQuest(Image quest)
 	{
 		quest.draw(100,100);
